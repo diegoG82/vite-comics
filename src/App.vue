366b8d5c2af4cf364720d3e3fc731758869d8c37
diegoG82 <diegogiardina@hotmail.it>
2023-04-13 14:21:56 +0200

    Project Scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script>
-export default {
-  data() {
-    return {
 
-    }
+import appHeader from "./components/appHeader.vue"
+import appMain from "./components/appMain.vue"
+import appFooter from "./components/appFooter.vue";
+
+export default {
+  components: {
+    appHeader,
+    appMain,
+    appFooter
+
+   
   }
 }
 
---
+++ src/components/appHeader.vue
@@ -0,0 +1,9 @@
+<script>
+export default{
+    name:"appHeader"
+}
+</script>
+<template>
+    <h1>header</h1>
+</template>
+<style scoped lang="scss"></style>
