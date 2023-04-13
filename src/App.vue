3b2531839825ab7571f14eef92badc4ea47cfc73
diegoG82 <diegogiardina@hotmail.it>
2023-04-13 14:10:47 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+
+
+
+</script>
+
+<template>
+  <h1>ciao</h1>
+
+
+</template>
+
+<style lang="scss">
+@use "./style/general.scss" as *;
+</style>
